fc01762558183014d6fcdde79e27bccd9af36ef0
isaacy2012 <50868057+isaacy2012@users.noreply.github.com>
2021-08-20 21:13:23 +1200

    Added IFR simulator

--- js/about.js
+++ js/about.js
@@ -2,3 +2,8 @@ document.getElementById("ferrari-video").addEventListener('ended', function(e) {
   e.target.currentTime = 0.25;
   e.target.play();
 }, false);
+
+document.getElementById("ifr-video").addEventListener('ended', function(e) {
+  e.target.currentTime = 0.0;
+  e.target.play();
+}, false);
